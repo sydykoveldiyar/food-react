ca75f5b327759d56f69151e256af78441b5664e0
Lamanris <sprite_kg@mail.ru>
2021-02-06 01:49:44 +0400

    Закончил dish-list

---
+++ src/components/waiter/dish-list/basket/basket.jsx
@@ -0,0 +1,88 @@
+import React from 'react';
+
+const Basket = () => {
+    return (
+        <section className="dish-list">
+            <div className="dish-list-item basket-item">
+                <h3 className="dish-list-item__title">Боул с черникой и гранолой</h3>
+                <div className="dish-list-item__bottom-side basket-item__bottom-side">
+                    <div className="dish-list-item__left-side">
+                        <div className="dish-list-item__price justify-align"><p>250,00</p></div>
+                        <button className="dish-list-item__plus-minus justify-align"><span>-</span></button>
+                        <div className="dish-list-item__count justify-align"><p>1</p></div>
+                        <button className="dish-list-item__plus-minus justify-align">+</button>
+                    </div>
+                    <div className="dish-list__btn-wrapper">
+                        <button className="dish-list__delete-btn">Удалить</button>
+                    </div>
+                </div>
+            </div>
+            <div className="dish-list-item basket-item">
+                <h3 className="dish-list-item__title">Боул с черникой и гранолой</h3>
+                <div className="dish-list-item__bottom-side basket-item__bottom-side">
+                    <div className="dish-list-item__left-side">
+                        <div className="dish-list-item__price justify-align"><p>250,00</p></div>
+                        <button className="dish-list-item__plus-minus justify-align"><span>-</span></button>
+                        <div className="dish-list-item__count justify-align"><p>1</p></div>
+                        <button className="dish-list-item__plus-minus justify-align">+</button>
+                    </div>
+                    <div className="dish-list__btn-wrapper">
+                        <button className="dish-list__delete-btn">Удалить</button>
+                    </div>
+                </div>
+            </div>
+            <div className="dish-list-item basket-item">
+                <h3 className="dish-list-item__title">Боул с черникой и гранолой</h3>
+                <div className="dish-list-item__bottom-side basket-item__bottom-side">
+                    <div className="dish-list-item__left-side">
+                        <div className="dish-list-item__price justify-align"><p>250,00</p></div>
+                        <button className="dish-list-item__plus-minus justify-align"><span>-</span></button>
+                        <div className="dish-list-item__count justify-align"><p>1</p></div>
+                        <button className="dish-list-item__plus-minus justify-align">+</button>
+                    </div>
+                    <div className="dish-list__btn-wrapper">
+                        <button className="dish-list__delete-btn">Удалить</button>
+                    </div>
+                </div>
+            </div>
+            <div className="dish-list-item basket-item">
+                <h3 className="dish-list-item__title">Боул с черникой и гранолой</h3>
+                <div className="dish-list-item__bottom-side basket-item__bottom-side">
+                    <div className="dish-list-item__left-side">
+                        <div className="dish-list-item__price justify-align"><p>250,00</p></div>
+                        <button className="dish-list-item__plus-minus justify-align"><span>-</span></button>
+                        <div className="dish-list-item__count justify-align"><p>1</p></div>
+                        <button className="dish-list-item__plus-minus justify-align">+</button>
+                    </div>
+                    <div className="dish-list__btn-wrapper">
+                        <button className="dish-list__delete-btn">Удалить</button>
+                    </div>
+                </div>
+            </div>
+            <div className="dish-list-item basket-item">
+                <h3 className="dish-list-item__title">Боул с черникой и гранолой</h3>
+                <div className="dish-list-item__bottom-side basket-item__bottom-side">
+                    <div className="dish-list-item__left-side">
+                        <div className="dish-list-item__price justify-align"><p>250,00</p></div>
+                        <button className="dish-list-item__plus-minus justify-align"><span>-</span></button>
+                        <div className="dish-list-item__count justify-align"><p>1</p></div>
+                        <button className="dish-list-item__plus-minus justify-align">+</button>
+                    </div>
+                    <div className="dish-list__btn-wrapper">
+                        <button className="dish-list__delete-btn">Удалить</button>
+                    </div>
+                </div>
+            </div>
+            <div className="dish-list-item basket-item basket-item__result">
+                <p className="basket-item__result-amount">4 товара</p>
+                <p className="basket-item__result-price">750,00</p>
+            </div>
+            <div className="dish-list__bottom-buttons">
+                <button className="dish-list__bottom-btn dish-list__accept-btn">Подтвердить</button>
+                <button className="dish-list__bottom-btn dish-list__add-btn">Добавить</button>
+            </div>
+        </section>
+    )
+};
+
+export default Basket;
