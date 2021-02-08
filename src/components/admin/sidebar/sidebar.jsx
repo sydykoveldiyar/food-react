7f302834293a7d311d57f65eb456034dcbbfcea1
Lamanris <sprite_kg@mail.ru>
2021-02-08 14:55:27 +0400

    Сделал весртку и адаптивность начальную

---
+++ src/components/admin/sidebar/sidebar.jsx
@@ -0,0 +1,29 @@
+import React from 'react';
+import {NavLink} from "react-router-dom";
+import './sidebar.css'
+
+const Sidebar = () => {
+    return (
+            <section className="admin-sidebar">
+                <div className="admin-sidebar__wrapper">
+                    <div className="admin-sidebar__container">
+                        <p className="admin-sidebar__logo">CRM Cafe</p>
+                        <div className="admin-sidebar__links">
+                            <NavLink to='#' className="admin-sidebar__link">Заказы</NavLink>
+                            <NavLink to='#' className="admin-sidebar__link">Продажи</NavLink>
+                            <NavLink to='#' className="admin-sidebar__link">Кухня</NavLink>
+                            <NavLink to='#' className="admin-sidebar__link">Бар</NavLink>
+                            <NavLink to='#' className="admin-sidebar__link">История транзакций</NavLink>
+                            <NavLink to='#' className="admin-sidebar__link">Пользователи</NavLink>
+                            <NavLink to='#' className="admin-sidebar__link">Категории</NavLink>
+                            <NavLink to='#' className="admin-sidebar__link">Список блюд</NavLink>
+                            <NavLink to='#' className="admin-sidebar__link">Столы</NavLink>
+                            <NavLink to='#' className="admin-sidebar__link">Бронирование</NavLink>
+                        </div>
+                    </div>
+                </div>
+            </section>
+    );
+};
+
+export default Sidebar;
